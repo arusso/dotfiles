f142c80d6ab89be628ffe75f147f31725e1fce2b
Aaron Russo <aaron.n.russo@gmail.com>
2014-04-18 20:31:57 -0700

    update alias for ll
    - stop sorting by modified time
    
    - setup colors

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,9 +1,11 @@
-alias ll='ls -hAlpt'
 
 if [[ "$PLATFORM"  == "OSX" ]]; then
+  alias ll='ls -hAlpG'
   alias lsusb='system_profiler SPUSBDataType'
   alias mygit='cd ~/git'
   alias truecrypt='/Applications/TrueCrypt.app/Contents/MacOS/Truecrypt --text'
+else
+  alias ll='ls -hAlp --color=auto'
 fi
 
 alias please='sudo !!'
