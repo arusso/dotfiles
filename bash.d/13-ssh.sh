62cb10419c8996fcb314a732f38110da70da444f
Aaron Russo <aaron.n.russo@gmail.com>
2015-08-25 21:31:56 -0700

    add environment variables to override ssh-agent/gpg-agent behavior
    * SSH_CACHE_TTL controls how long the ssh keys are cached
    * USE_GPG_AGENT will disable using gpg-agent when set to 0

--- bash.d/13-ssh.sh
+++ bash.d/13-ssh.sh
@@ -1,19 +1,27 @@
+# default to using gpg-agent
+GPG_AGENT=$(which gpg-agent)
+USE_GPG_AGENT=${USE_GPG_AGENT:=1}
+
+# we can't use gpg agent if it's not installed
+if test -z "$GPG_AGENT"; then USE_GPG_AGENT=0; fi
+export GPG_AGENT
+export USE_GPG_AGENT
+
 # if we have gpg-agent installed, and we don't see $HOME/.use_ssh_agent, then
 # load gpg-agent. otherwise load ssh-agent
 _gpg_agent=$(which gpg-agent)
-if [ $? -eq 1 ] || [[ -f "$HOME/.use_ssh_agent" ]]; then
-  export USE_GPG_AGENT=0
-else
-  export USE_GPG_AGENT=1
-fi
+if [ $? -eq 1 ]; then USE_GPG_AGENT=0; fi
+
+export USE_GPG_AGENT
 
 setup_ssh_agent() {
+  SSH_CACHE_TTL=${SSH_CACHE_TTL:=1800}
   _ssh_auth_sock="$HOME/.ssh_auth_sock"
   _ssh_agent_info="$HOME/.ssh_agent_info"
   if test -L "$_ssh_auth_sock" && kill -0 $(head -n 1 "$_ssh_agent_info" | cut -d= -f2) 2>/dev/null; then
     . "$_ssh_agent_info"
   else
-    eval $(ssh-agent -s -t 3600) >/dev/null
+    eval $(ssh-agent -s -t $SSH_CACHE_TTL) >/dev/null
     rm -f $_ssh_auth_sock
     ln -sf $SSH_AUTH_SOCK $_ssh_auth_sock
     echo "SSH_AGENT_PID=$SSH_AGENT_PID" > $_ssh_agent_info
@@ -25,13 +33,14 @@ setup_ssh_agent() {
 
 setup_gpg_agent() {
   export GPG_TTY=$(tty)
+  SSH_CACHE_TTL=${SSH_CACHE_TTL:=1800}
   _gpg_agent_info="$HOME/.gpg_agent_info"
   _ssh_auth_sock="$HOME/.ssh_auth_sock"
   # test to see if we have an existing agent loaded, and load gpg-agent if not
   if test -f "$_gpg_agent_info" && kill -0 $(head -n 1 "$_gpg_agent_info" | cut -d: -f2) 2>/dev/null ; then
     eval $(< "$_gpg_agent_info")
   else
-    eval $(gpg-agent --daemon --enable-ssh-support --write-env-file "$_gpg_agent_info")
+    eval $(gpg-agent --daemon --enable-ssh-support --write-env-file "$_gpg_agent_info" --default-cache-ttl-ssh $SSH_CACHE_TTL)
   fi
   export GPG_AGENT_INFO
   rm -f $_ssh_auth_sock
