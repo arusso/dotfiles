079cb2b31490638293cd0b88bef7fd74846c6c79
Aaron Russo <arusso@berkeley.edu>
2015-11-19 14:14:50 -0800

    place a warning to ensure we do not accidentally store sensitive data in vcs

--- bash.d/99-private.sh
+++ bash.d/99-private.sh
@@ -1,2 +1,11 @@
+################################################################################
+## WARNING ## WARNING ## WARNING ## WARNING ## WARNING ## WARNING ## WARNING ###
+################################################################################
+# THIS FILE IS STORED IN VERSION CONTROL AND IS NOT SAFE FOR STORING SENSITIVE #
+# INFORMATION.                                                                 #
+################################################################################
+## WARNING ## WARNING ## WARNING ## WARNING ## WARNING ## WARNING ## WARNING ###
+################################################################################
+
 # handle the old way of loading non-checked in configurations
 [[ -f $HOME/.bashrc.private ]] && . "$HOME/.bashrc.private"
