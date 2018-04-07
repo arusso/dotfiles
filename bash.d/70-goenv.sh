75d9a40683b62e5eaaaa65de3a8f268874b3065b
Aaron Russo <aaron.n.russo@gmail.com>
2018-04-06 21:02:11 -0700

    ensure we have goenv installed before trying to initialize

--- bash.d/70-goenv.sh
+++ bash.d/70-goenv.sh
@@ -1,6 +1,6 @@
 export GOPATH="$HOME/.config/go"
 
-if [ -d "$HOME/.goenv" ]; then
+if [ -d "$HOME/.goenv" ] && [ "$(which goenv 2>/dev/null)" != "" ]; then
   export GOENV_ROOT="$HOME/.goenv"
   export PATH="$GOENV_ROOT/bin:$PATH"
   eval "$(goenv init -)"
