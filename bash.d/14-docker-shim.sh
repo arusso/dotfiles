0d2907c31dd16e562b196864bee9b18950ed36b2
Aaron Russo <aaron.n.russo@gmail.com>
2018-11-05 10:53:19 -0800

    hide test output

--- bash.d/14-docker-shim.sh
+++ bash.d/14-docker-shim.sh
@@ -1,4 +1,4 @@
-if [[ $SHIM_DOCKER_ENABLE -eq 1 && $(which docker) ]]; then
+if [[ $SHIM_DOCKER_ENABLE -eq 1 && $(which docker 2>/dev/null) ]]; then
   docker ()
   {
     [[ $UID -ne 0 ]] && DOCKER="sudo docker" || DOCKER="docker";
