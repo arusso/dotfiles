7859d1bef3645b7d40980edc991bed05d922ded0
Aaron Russo <aaron.n.russo@gmail.com>
2018-11-09 11:29:32 -0800

    set fixed GOPATH, add alias for go get

--- bash.d/70-golang.sh
+++ bash.d/70-golang.sh
@@ -1,6 +1,7 @@
 #
 # Go Language Configuration
 GOENV="$(which goenv 2>/dev/null)"
+GOPATH="$HOME/go"
 if [[ -x "$GOENV" ]] && [[ -d "$HOME/.goenv" ]]; then
   export GOENV_ROOT="$HOME/.goenv"
   export PATH="$GOENV_ROOT/bin:$PATH"
@@ -8,3 +9,5 @@ if [[ -x "$GOENV" ]] && [[ -d "$HOME/.goenv" ]]; then
 elif [[ -d "$GO" ]]; then
   export PATH="$PATH:$(go env GOPATH)/bin"
 fi
+
+alias gg="go get -d"
