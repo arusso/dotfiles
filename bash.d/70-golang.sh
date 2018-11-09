de15c2e3f56c7b2068d7e0a1666ecf1b86b947bc
Aaron Russo <aaron.n.russo@gmail.com>
2018-11-08 22:49:50 -0800

    add goenv back into the mix

--- bash.d/70-golang.sh
+++ bash.d/70-golang.sh
@@ -1,16 +1,10 @@
 #
 # Go Language Configuration
-
-export GOPATH="$HOME/go"
-GO="$(which go 2>/dev/null)"
-
-if [ "$GO" != "" ]; then
+GOENV="$(which goenv 2>/dev/null)"
+if [[ -x "$GOENV" ]] && [[ -d "$HOME/.goenv" ]]; then
+  export GOENV_ROOT="$HOME/.goenv"
+  export PATH="$GOENV_ROOT/bin:$PATH"
+  eval "$(goenv init -)"
+elif [[ -d "$GO" ]]; then
   export PATH="$PATH:$(go env GOPATH)/bin"
 fi
-
-# configure goenv
-#if [ -d "$HOME/.goenv" ] && [ "$(which goenv 2>/dev/null)" != "" ]; then
-#  export GOENV_ROOT="$HOME/.goenv"
-#  export PATH="$GOENV_ROOT/bin:$PATH"
-#  eval "$(goenv init -)"
-#fi
