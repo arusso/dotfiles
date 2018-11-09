265af02571d05b487c238d248b4da622193bfe41
Aaron Russo <aaron.n.russo@gmail.com>
2018-11-08 21:36:13 -0800

    remove old puppet test code... ):

--- bash.d/13-functions.sh
+++ bash.d/13-functions.sh
@@ -65,55 +65,6 @@ function check_erb() {
   erb -P -x -T '-' $1 | ruby -c
 }
 
-
-# iterates through a file of hostnames, one per line, and tests them against a
-# specified environment name
-#
-# usage: test_puppet_branch <hosts file> <environment name>
-function test_puppet_branch() {
-  _filename="$1"
-  _environment="$2"
-  _usage="usage: FILENAME ENVIRONMENT
-
-    FILENAME - name of file containing hosts to test
-    ENVIRONMENT - puppet environment to test"
-
-  # verify our filename
-  if ! /bin/test -f "$_filename"; then
-    echo "invalid file specified, '${_filename}'" 1>&2
-    echo 1>&2
-    echo "$_usage" 1>&2
-    return 1
-  fi
-
-  # make sure we passed something for the environment
-  if /bin/test -z "${_environment}"; then
-    echo "you must specify an environment!" 1>&2
-    echo 1>&2
-    echo "$_usage" 1>&2
-    return 1
-  fi
-
-  # stay in the loop
-  echo "beginning testing of environment '${_environment}'"
-  echo "reading from file '${_filename}'"
-  echo "press CTRL-C to stop..."
-  while [ 1 -eq 1 ]; do
-    # read each line
-    while read line; do
-      for hst in $(eval "echo $line"); do 
-        echo "#### HOST: ${hst} ####"
-        runon ${hst} "sudo puppet agent -t --noop --environment ${_environment}"
-        echo "#######################"
-      done
-      # delete the entry from our file
-      eval "sed -i '' -e '/^$line\$/d'" $_filename
-    done < $_filename
-    # sleep afterwards to prevent spinning our wheels
-    sleep 1
-  done
-}
-
 get_abs_filename() {
   # $1 : relative filename
   echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
