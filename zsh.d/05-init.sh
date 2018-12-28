3d4a4b378a57339ff085a18d3eabd6974a88f7c8
Aaron Russo <aaron.n.russo@gmail.com>
2018-12-28 12:58:42 -0800

    update zsh config

---
+++ zsh.d/05-init.sh
@@ -0,0 +1,21 @@
+# misc config
+export LC_ALL="en_US.UTF-8"
+export CLICOLOR='Yes'
+
+# history config
+setopt append_history     # multiple sessions share a file
+setopt extended_history   # save timestamp and elapsed time
+setopt hist_find_no_dups  # do not find duplicate commands
+setopt hist_reduce_blanks # dont store blank lines
+setopt inc_append_history # append as commands are called
+setopt share_history      # share a history file
+setopt hist_no_store      # do not store calls to the history cmd
+
+[[ -d "$HOME/.zsh_history" ]] && mkdir $HOME/.zsh_history
+HISTFILE="$HOME/.zsh_history/$HOSTNAME-$(date +Ymd)"
+HISTSIZE=500
+SAVEHIST=200000
+
+# ls config
+export LSCOLORS='Exgxfxcxdxdxhbadbxbx'
+export LS_OPTIONS='--color=auto'
---
+++ zsh.d/11-aliases.sh
@@ -0,0 +1,4 @@
+# alias definitions
+alias ls='ls -ahF'
+alias ll='ls -alhF'
+
