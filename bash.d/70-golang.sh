873a53ebaeaa4e3613d154dba08c9d666ef51e24
Aaron Russo <aaron.n.russo@gmail.com>
2019-01-22 13:04:08 -0800

    update golang env setup

--- bash.d/70-golang.sh
+++ bash.d/70-golang.sh
@@ -1,12 +1,13 @@
 #
 # Go Language Configuration
+GO=$(which go)
 GOENV="$(which goenv 2>/dev/null)"
 GOPATH="$HOME/go"
 if [[ -x "$GOENV" ]] && [[ -d "$HOME/.goenv" ]]; then
   export GOENV_ROOT="$HOME/.goenv"
   export PATH="$GOENV_ROOT/bin:$PATH"
   eval "$(goenv init -)"
-elif [[ -d "$GO" ]]; then
+elif [[ -d "$GOPATH" ]] && [[ -x "$GO" ]]; then
   export PATH="$PATH:$(go env GOPATH)/bin"
 fi
 
