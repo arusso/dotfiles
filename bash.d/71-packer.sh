f1dfe994e88c0aeec9fc3c7837a1ed433d9cb8e9
Aaron Russo <aaron.n.russo@gmail.com>
2019-02-28 20:48:55 -0800

    move packer cache, only setup env if packer is installed

--- bash.d/71-packer.sh
+++ bash.d/71-packer.sh
@@ -1,3 +1,6 @@
-# make sure we use a single packer cache directory so we can minimize the space
-# we take up
-export PACKER_CACHE_DIR="$HOME/packer_cache"
+if [[ $(:binexists packer) -eq 0 ]]; then
+  # make sure we use a single packer cache directory so we can minimize the
+  # space we take up
+  export PACKER_CACHE_DIR="$HOME/.packer/cache"
+  [[ ! -d "$PACKER_CACHE_DIR" ]] && mkdir -p "$PACKER_CACHE_DIR"
+fi
