95c956aea4c46b79caf0fca5cc63718442db3501
Aaron Russo <aaron.n.russo@gmail.com>
2019-02-28 20:22:49 -0800

    fix path management for golang w/goenv and rbenv

--- bash.d/70-golang.sh
+++ bash.d/70-golang.sh
@@ -5,12 +5,10 @@ GOENV="$(which goenv 2>/dev/null)"
 GOPATH="$HOME/go"
 if [[ -x "$GOENV" ]] && [[ -d "$HOME/.goenv" ]]; then
   export GOENV_ROOT="$HOME/.goenv"
-  :appendpath "$GOENV/bin"
-  #export PATH="$GOENV_ROOT/bin:$PATH"
-  eval "$(goenv init -)"
+  :prependpath "$GOENV/bin"
+  [[ $(:inpath "$HOME/.goenv/shims") -eq 1 ]] && eval "$(goenv init -)"
 elif [[ -d "$GOPATH" ]] && [[ -x "$GO" ]]; then
-  #export PATH="$PATH:$(go env GOPATH)/bin"
-  :appendpath "$(go env GOPATH)/bin"
+  :prependpath "$(go env GOPATH)/bin"
 fi
 
 alias gg="go get -d"
--- bash.d/70-rbenv.sh
+++ bash.d/70-rbenv.sh
@@ -1,5 +1,5 @@
 # initialize rbenv if boxen isn't installed
 if [[ -z "$BOXEN_HOME" ]]; then
   :prependpath "$HOME/.rbenv/bin"
-  [[ $(:inpath "$HOME/.rbenv/shims") -eq 1 ]] && eval "$(rbenv init -)"
+  [[ $(:binexists rbenv) -eq 0 && $(:inpath "$HOME/.rbenv/shims") -eq 1 ]] && eval "$(rbenv init -)"
 fi
