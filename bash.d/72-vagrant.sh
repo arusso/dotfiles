372e935907f2fe1a893d962ca4a52cb6b730539e
Aaron Russo <aaron.n.russo@gmail.com>
2019-02-28 20:49:12 -0800

    only setup environment if vagrant is installed

--- bash.d/72-vagrant.sh
+++ bash.d/72-vagrant.sh
@@ -1,4 +1,6 @@
-alias v='vagrant'
-alias vd='vagrant destroy'
-alias vu='vagrant up'
-alias vs='vagrant ssh'
+if [[ $(:binexists vagrant) -eq 0 ]]; then
+  alias v='vagrant'
+  alias vd='vagrant destroy'
+  alias vu='vagrant up'
+  alias vs='vagrant ssh'
+fi
