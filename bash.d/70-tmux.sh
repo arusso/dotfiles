90154c9f7e217dfedc14941cf596db49114d82b9
Aaron Russo <aaron.n.russo@gmail.com>
2019-04-15 19:01:05 -0700

    tmux: Warn when fzf isn't installed

--- bash.d/70-tmux.sh
+++ bash.d/70-tmux.sh
@@ -12,5 +12,10 @@ if test -z "$TMUXIFIER_INIT"; then
   export TMUXIFIER_INIT=1
 fi
 
+# we need fzf so we can do fancy window searching
+if [[ $(:binexists fzf) -ne 0 ]]; then
+  echo "warning: please install fzf for tmux window searching to function"
+fi
+
 # resume tmux when it accidentally gets backgrounded via ctrl-z
 fix_tmux() { pkill -CONT tmux; }
