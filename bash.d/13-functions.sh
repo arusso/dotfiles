c4cdeb4c161bb44afcc6ea85cfe3023584aedcaa
Aaron Russo <aaron.n.russo@gmail.com>
2019-04-15 19:00:20 -0700

    Pipe stderr to /dev/null

--- bash.d/13-functions.sh
+++ bash.d/13-functions.sh
@@ -99,6 +99,6 @@ epoch2date() {
 # normalize a mac address to form EUI64 form.
 nmac ()
 {
-  [[ "$PLATFORM" -eq "OSX" ]] && SED=$(which gsed) || SED=$(which sed)
+  [[ "$PLATFORM" -eq "OSX" ]] && SED=$(which gsed 2>/dev/null) || SED=$(which sed 2>/dev/null)
   echo $1 | $SED -e 's/[.:]//g;s/.\{2\}/&:/g;s/:$//;s/./\U&/g'
 }
