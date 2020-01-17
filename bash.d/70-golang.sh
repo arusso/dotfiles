93e55a7f8be7f2706b1ef284592d6be705675e2b
Aaron Russo <aaron.n.russo@gmail.com>
2020-01-17 10:43:41 -0800

    golang: Ensure goenv() is defined

--- bash.d/70-golang.sh
+++ bash.d/70-golang.sh
@@ -7,6 +7,22 @@ if [[ ! -z "$GO" ]]; then
     :prependpath "$HOME/.goenv/bin"
     GOENV_ROOT="$HOME/.goenv"
     [[ $(:inpath "$HOME/.goenv/shims") -eq 1 ]] && eval "$(goenv init -)"
+
+    # enable shell integration
+    goenv() {
+      local command
+      command="$1"
+      if [ "$#" -gt 0 ]; then
+        shift
+      fi
+
+      case "$command" in
+      rehash|shell)
+        eval "$(goenv "sh-$command" "$@")";;
+      *)
+        command goenv "$command" "$@";;
+      esac
+    }
   elif [[ -d "$GOPATH" ]] && [[ -x "$GO" ]]; then
     :prependpath "$(go env GOPATH)/bin"
   fi
