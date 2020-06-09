60605541b02a9f79aa04857f7f50525cc4cc555c
Aaron Russo <aaron.n.russo@gmail.com>
2020-06-09 11:15:41 -0700

    zsh: bring over pyenv config

---
+++ zsh.d/70-pyenv.sh
@@ -0,0 +1,27 @@
+# initialize pyenv
+if [[ -z "$PYENV_ROOT" ]] && [[ -d $HOME/.pyenv ]]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+fi
+:prependpath "$PYENV_ROOT/bin"
+
+if [[ $(:binexists pyenv) -eq 0 ]]; then
+  if [[ $(:inpath "$HOME/.pyenv/shims") -eq 1 ]]; then
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init - 2>/dev/null)"
+  fi
+fi
+
+pyenv() {
+  local command
+  command="${1:-}"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  activate|deactivate|rehash|shell)
+    eval "$(pyenv "sh-$command" "$@")";;
+  *)
+    command pyenv "$command" "$@";;
+  esac
+}
