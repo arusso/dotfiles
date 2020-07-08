59466cc70f3ee1a97a5f572ef117b93dd3465898
Aaron Russo <aaron.n.russo@gmail.com>
2020-07-07 21:05:10 -0700

    Fix ls/ll aliases on BSD/macOS systems

--- zsh.d/11-aliases.sh
+++ zsh.d/11-aliases.sh
@@ -1,3 +1,10 @@
 # alias definitions
-alias ls='ls --color=auto -ahF'
-alias ll='ls --color=auto -alhF'
+
+# bsd vs gnu utilities
+if [[ "$(uname)" == "Linux" ]]; then
+  alias ls='ls --color=auto -ahF'
+  alias ll='ls --color=auto -alhF'
+else
+  alias ls='ls -ahGF'
+  alias ll='ls -alhGF'
+fi
