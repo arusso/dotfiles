aea81826889cf8653563d7b011edb1bd54e8b226
Aaron Russo <aaron.n.russo@gmail.com>
2020-10-26 12:49:57 -0700

    zsh: History should ignore lines with leading space

--- zsh.d/05-init.sh
+++ zsh.d/05-init.sh
@@ -10,6 +10,8 @@ setopt hist_reduce_blanks # dont store blank lines
 setopt inc_append_history # append as commands are called
 setopt share_history      # share a history file
 setopt hist_no_store      # do not store calls to the history cmd
+setopt histignorespace    # ignore entries with with leading whitspace
+
 
 [[ ! -d "$HOME/.zsh_history" ]] && mkdir "$HOME"/.zsh_history
 HISTFILE="$HOME/.zsh_history/$HOSTNAME-$(date +%Y%m%d)"
