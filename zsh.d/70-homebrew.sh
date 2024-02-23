8e4c1a8eda41e6858beb04bd4a019111d58e88a7
Aaron Russo <aaron.n.russo@gmail.com>
2024-02-22 21:36:30 -0800

    feat: homebrew on arm

--- zsh.d/70-homebrew.sh
+++ zsh.d/70-homebrew.sh
@@ -2,4 +2,9 @@
 # and a 10 second install often turns into a 30m upgrade.
 if [[ "$PLATFORM" == "OSX" ]]; then
   HOMEBREW_NO_AUTO_UPDATE=1
+
+  # On Apple Silicon, homebrew has a new path
+  if [[ "$(uname -p)" == 'arm' && -d '/opt/homebrew' ]]; then
+    :appendpath '/opt/homebrew/bin'
+  fi
 fi
