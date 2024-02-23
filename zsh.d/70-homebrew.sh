b059d5098719d4529347eaffa4acc018c612e93f
Aaron Russo <aaron.n.russo@gmail.com>
2024-02-22 21:37:06 -0800

    change: disable homebrew env hints

--- zsh.d/70-homebrew.sh
+++ zsh.d/70-homebrew.sh
@@ -2,6 +2,7 @@
 # and a 10 second install often turns into a 30m upgrade.
 if [[ "$PLATFORM" == "OSX" ]]; then
   HOMEBREW_NO_AUTO_UPDATE=1
+  HOMEBREW_NO_ENV_HINTS=1
 
   # On Apple Silicon, homebrew has a new path
   if [[ "$(uname -p)" == 'arm' && -d '/opt/homebrew' ]]; then
