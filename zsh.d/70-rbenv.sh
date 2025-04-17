1b08a433251a496ce8bfddd9d153e6a4c4b81f20
Aaron Russo <aaron.n.russo@gmail.com>
2025-04-17 00:32:25 -0700

    change(ruby): Disable rbenv by default

--- zsh.d/70-rbenv.sh
+++ zsh.d/70-rbenv.sh
@@ -1,17 +1,25 @@
-:prependpath "$HOME/.rbenv/bin"
-[[ $(:binexists rbenv) -eq 0 && $(:inpath "$HOME/.rbenv/shims") -eq 1 ]] && eval "$(rbenv init -)"
+if [[ ${RBENV_ENABLE:-0} -eq 1 ]]; then
+  export RBENV_ROOT="${RBENV_ROOT:-$HOME/.rbenv}"
+  export RBENV_SHELL=zsh
+  source "$RBENV_ROOT/completions/rbenv.zsh"
 
-rbenv() {
-  local command
-  command="${1:-}"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
+  :appendpath "$RBENV_ROOT/shims"
+  :appendpath "$RBENV_ROOT/bin"
+  #[[ $(:binexists rbenv) -eq 0 && $(:inpath "$HOME/.rbenv/shims") -eq 1 ]] && eval "$(rbenv init -)"
 
-  case "$command" in
-    rehash|shell)
-      eval "$(rbenv "sh-$command" "$@")";;
-    *)
-      command rbenv "$command" "$@";;
-  esac
-}
+
+  rbenv() {
+    local command
+    command="${1:-}"
+    if [ "$#" -gt 0 ]; then
+      shift
+    fi
+
+    case "$command" in
+      rehash|shell)
+        eval "$(rbenv "sh-$command" "$@")";;
+      *)
+        command rbenv "$command" "$@";;
+    esac
+  }
+fi
