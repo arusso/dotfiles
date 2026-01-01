1016413019b48f5f0c909a791094f8033c867d04
Aaron Russo <aaron.n.russo@gmail.com>
2025-12-31 16:23:15 -0800

    fix(zshrc): Error when node is not installed

--- zsh.d/70-nodenv.sh
+++ zsh.d/70-nodenv.sh
@@ -2,23 +2,27 @@
 if [[ -z "$NODENV_ROOT" ]] && [[ -d $HOME/.nodenv ]]; then
   export NODENV_ROOT="$HOME/.nodenv"
 fi
-:appendpath "$NODENV_ROOT/bin"
-:appendpath "$NODENV_ROOT/shims"
 
-export NODENV_SHELL=zsh
-source "$NODENV_ROOT/libexec/../completions/nodenv.zsh"
-command nodenv rehash 2>/dev/null
-nodenv() {
-  local command
-  command="${1:-}"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
+if [[ ! -z "$NODENV_ROOT" ]]; then
+  :appendpath "$NODENV_ROOT/bin"
+  :appendpath "$NODENV_ROOT/shims"
 
-  case "$command" in
-  rehash|shell)
-    eval "$(nodenv "sh-$command" "$@")";;
-  *)
-    command nodenv "$command" "$@";;
-  esac
-}
+  export NODENV_SHELL=zsh
+  source "$NODENV_ROOT/libexec/../completions/nodenv.zsh"
+  command nodenv rehash 2>/dev/null
+  nodenv() {
+    local command
+    command="${1:-}"
+    if [ "$#" -gt 0 ]; then
+      shift
+    fi
+
+    case "$command" in
+    rehash|shell)
+      eval "$(nodenv "sh-$command" "$@")";;
+    *)
+      command nodenv "$command" "$@";;
+    esac
+  }
+fi
+
